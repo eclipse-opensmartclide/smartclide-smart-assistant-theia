c270bb290c6180ba3cc8f7f06f76ac50419e600f
zakieh <zakiehalizadeh@gmail.com>
2023-02-13 13:20:51 +0100

    Add missed copyright

--- iamodeler/iamodeler/src/browser/configuration.ts
+++ iamodeler/iamodeler/src/browser/configuration.ts
@@ -1,4 +1,14 @@
 
+/*******************************************************************************
+ * Copyright (C) 2021-2022 AIR Institute
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
 // export const SMARTCLIDE_URL = "http://vm-bisite-57.der.usal.es";
 // export const SMARTCLIDE_TARGET_MODULE_PATH = "/smartclide/v1/iamodeler";
 // export const SMARTCLIDE_PORT = '5001';
--- iamodeler/iamodeler/src/browser/iamodeler-widget.tsx
+++ iamodeler/iamodeler/src/browser/iamodeler-widget.tsx
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2021-2022 AIR Institute
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 import * as React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ModelsInfo from "./models_info";
